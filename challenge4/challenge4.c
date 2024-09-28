4939effa4b54802ae93fd246547b5c2780932c5a
Yasmineebh1 <bahiyasmine.13@gmail.com>
2024-09-28 10:52:39 +0200

    update

--- challenge4/challenge4.c
+++ challenge4/challenge4.c
@@ -1,23 +1,32 @@
 #include<stdio.h>
 
 int main(){
-    int t[100],i,a,max;
+    int t[10],i,a,maxim,minim;
 
-    
-    for(i = 0; i < 5; i++){
-         printf("entrer entier %d:",i+1);
-            scanf("%d",&t[i]);}
 
-    
-        for(i = 0; i < 5; i++)
-        {
-            if(t[i + 1] > t[i]){
-                printf("le plus petit est %d",t[i]);
-                break;
-                
-            }
-            else
-                printf("le plus grand est %d",t[i]);
+        
+    for(i = 0;i < 5;i++){
+        printf("entrer entier nr %d:", i+1);
+        scanf("%d",&t[i]);
+    }
+    maxim = t[0];
+   
+    for(i = 1;i < 5; i++){
+        if(maxim > t[i]){
+            maxim = t[i];
+            
         }
-
-}
+       
+        
+    }
+     printf("le plus grand est:%d\n",maxim);
+     for(i = 1;i < 5; i++){
+        if(minim < t[i]){
+            minim = t[i];
+            
+        }
+       
+        
+    }
+     printf("le plus petit est:%d",minim);
+   
